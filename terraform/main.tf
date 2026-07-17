adb9e3a525916250a3eb38683f98d80df0ca35e2
megdewan18 <megha.dewan18@gmail.com>
2026-07-18 00:15:46 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,20 +51,18 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.24.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = var.cluster_version
+  name    = local.cluster_name
+  kubernetes_version = var.cluster_version
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2023_x86_64_STANDARD"
-  }
+  endpoint_public_access = true
 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
+
+      ami_type = "AL2023_x86_64_STANDARD"
 
       instance_types = ["t3.small"]
 
@@ -75,6 +73,8 @@ module "eks" {
 
     two = {
       name = "node-group-2"
+
+      ami_type = "AL2023_x86_64_STANDARD"
 
       instance_types = ["t3.small"]
 
